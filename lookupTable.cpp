62b37d755d4c0b177307c264c0adf6836c85cd01
Matthew Todd <matthewdb.todd@gmail.com>
2019-08-19 17:22:33 +0100

    Made score table and refactored functions to be smaller

--- lookupTable.cpp
+++ lookupTable.cpp
@@ -6,6 +6,7 @@
 
 uint16_t forwardTable[65536];
 uint16_t reverseTable[65536]; 
+int scoreTable[65536];
 uint16_t resetMasks[4] {
     0xFFF0,
     0xFF0F,
@@ -14,7 +15,7 @@ uint16_t resetMasks[4] {
 };
 
 int getMask(const uint16_t row, const int pos) {
-    return (row & (0xF << (pos << 2))) >> (pos << 2);
+    return (row & ~resetMasks[pos]) >> (pos << 2);
 }
 
 template <typename T>
@@ -41,7 +42,7 @@ uint16_t swap(uint16_t row, int bitNum, int value, T& move) {
 }
 
 template <typename T>
-uint16_t generateRowAndScore(uint16_t row, T& move) {
+uint16_t generateRow(uint16_t row, T& move) {
     for (int i {move.start}; move.end(i); i = move.next(i)) {
         int value {getMask(row, i)};
         if (!value) {
@@ -59,14 +60,30 @@ uint16_t generateRowAndScore(uint16_t row, T& move) {
     return row;
 }
 
+int generateScore(uint16_t row) {
+    int score {0};
+    char lastValue {0};
+    for (int i {0}; i < 4; ++i) {
+        char value = ((0xF << (i*4)) & row) >> (i*4);
+        if (value == 0) { continue; }
+        if (value == lastValue) {
+            score += (0x1 << (value+1));
+            lastValue = 0;
+        } else {
+            lastValue = value; 
+        }
+    }
+    return score;
+}
 
 void populateTables() {
     for (int i {0}; i < 65536; ++i) {
         Forward f;
         Reverse r;
         uint16_t row {static_cast<uint16_t>(i)};
-        forwardTable[i] = generateRowAndScore(row, f); 
-        reverseTable[i] = generateRowAndScore(row, r);
+        forwardTable[i] = generateRow(row, f); 
+        reverseTable[i] = generateRow(row, r);
+        scoreTable[i] = generateScore(row);
     }
 }
 
@@ -77,7 +94,8 @@ int main() {
     for (int i {0}; i < 65536; ++i) {
         out << std::setfill('0') << std::setw(4) << std::hex << i << '\n';
         out << "\tForward --> " << std::setfill('0') << std::setw(4) << std::hex << forwardTable[i] << '\n';
-        out << "\tReverse --> " << std::setfill('0') << std::setw(4) << std::hex << reverseTable[i] << "\n\n";
+        out << "\tReverse --> " << std::setfill('0') << std::setw(4) << std::hex << reverseTable[i] << '\n';
+        out << "\tScore ----> " << std::dec << scoreTable[i] << "\n\n";
     }
     return 0;
 }
