6a25db6fcd01c506bad3980d5a72a01da8d3b48a
Matthew Todd <matthewdb.todd@gmail.com>
2019-08-19 16:21:37 +0100

    populate forward and reverse tables with template

--- lookupTable.cpp
+++ lookupTable.cpp
@@ -1,8 +1,11 @@
 #include <cstdint>
 #include <iostream>
+#include <iomanip>
+#include <fstream>
+#include "Move_.h"
 
 uint16_t forwardTable[65536];
-uint16_t reverseTable[65536];
+uint16_t reverseTable[65536]; 
 uint16_t resetMasks[4] {
     0xFFF0,
     0xFF0F,
@@ -14,144 +17,67 @@ int getMask(const uint16_t row, const int pos) {
     return (row & (0xF << (pos << 2))) >> (pos << 2);
 }
 
-// somehow this shit works
-void populateForwardTable() {
-    forwardTable[0] = 0;
-    for (int i {1}; i < 65536; ++i) {
-        // row
-        int nextEmptyPosition {-1};
-        int lastValue {-1};
-        int lastValueIndex {-1};
-        uint16_t row {static_cast<uint16_t>(i)};
-
-        //std::cout << "Initial row --> " << std::hex << row << '\n';
-
-        for (int j {0}; j < 4; ++j) {
-            int value {getMask(row, j)};
-            if (!value) {
-                nextEmptyPosition = (nextEmptyPosition == -1) ? j : nextEmptyPosition;
-            } else if (value == lastValue) {
-                lastValue = (lastValue == 0xF) ? 0xF : lastValue+1;
-                // zero tile first
-                row &= resetMasks[j];
-                row &= resetMasks[lastValueIndex];
-
-                //std::cout << "Row after zeroing both tiles before merging --> " << std::hex << row << '\n';
-
-                // set to incremented value
-                row |= (lastValue << (lastValueIndex << 2));
-
-                //std::cout << "Row after merging --> " << std::hex << row << '\n';
-
-                nextEmptyPosition = lastValueIndex+1;
-                lastValue = -1;
-                lastValueIndex = -1;
-            } else {
-                lastValue = value;
-                lastValueIndex = j;
-                if (nextEmptyPosition != -1) {
-                    row |= value << (nextEmptyPosition * 4);
-                    row &= resetMasks[j];
-                    //
-                    //value >>= (j - lastValueIndex)*4;
-                    //row |= value;
-                    lastValueIndex = nextEmptyPosition;
-                    ++nextEmptyPosition;
-                }
-            }
-        }        
-        forwardTable[i] = row;
-        std::cout << std::hex << i << " --> " << forwardTable[i] << '\n';
-    }
+template <typename T>
+uint16_t merge(uint16_t row, int bitNum, T& move) {
+    move.lastValue = (move.lastValue == 0xF) ? 0xF : move.lastValue+1;
+    // zero tile
+    row &= resetMasks[bitNum];
+    row &= resetMasks[move.lastValueIndex];
+    // set to incremented value
+    row |= (move.lastValue << (move.lastValueIndex << 2));
+    move.nextEmptyPosition = move.next(move.lastValueIndex);
+    move.lastValue = -1;
+    move.lastValueIndex = -1;
+    return row;
 }
 
-void populateReverseTable() {
-    reverseTable[0] = 0;
-    for (int i {1}; i < 65536; ++i) {
-        // row
-        int nextEmptyPosition {-1};
-        int lastValue {-1};
-        int lastValueIndex {-1};
-        uint16_t row {static_cast<uint16_t>(i)};
-
-        //std::cout << "Initial row --> " << std::hex << row << '\n';
-
-        for (int j {3}; j >= 0; --j) {
-            int value {getMask(row, j)};
-            if (!value) {
-                nextEmptyPosition = (nextEmptyPosition == -1) ? j : nextEmptyPosition;
-            } else if (value == lastValue) {
-                lastValue = (lastValue == 0xF) ? 0xF : lastValue+1;
-                // zero tile first
-                row &= resetMasks[j];
-                row &= resetMasks[lastValueIndex];
-
-                //std::cout << "Row after zeroing both tiles before merging --> " << std::hex << row << '\n';
-
-                // set to incremented value
-                row |= (lastValue << (lastValueIndex << 2));
-
-                //std::cout << "Row after merging --> " << std::hex << row << '\n';
-
-                nextEmptyPosition = lastValueIndex-1;
-                lastValue = -1;
-                lastValueIndex = -1;
-            } else {
-                lastValue = value;
-                lastValueIndex = j;
-                if (nextEmptyPosition != -1) {
-                    row |= value << (nextEmptyPosition * 4);
-                    row &= resetMasks[j];
-                    //
-                    //value >>= (j - lastValueIndex)*4;
-                    //row |= value;
-                    lastValueIndex = nextEmptyPosition;
-                    --nextEmptyPosition;
-                }
-            }
-        }        
-        reverseTable[i] = row;
-        std::cout << std::hex << i << " --> " << reverseTable[i] << '\n';
-    }
-
+template <typename T>
+uint16_t swap(uint16_t row, int bitNum, int value, T& move) {
+    row |= value << (move.nextEmptyPosition * 4);
+    row &= resetMasks[bitNum];
+    move.lastValueIndex = move.nextEmptyPosition;
+    move.nextEmptyPosition = move.next(move.nextEmptyPosition);
+    return row;
 }
 
-// bugged as shit
-void printMoveRow() {
-    for (uint16_t i {0}; i < 65536; ++i) {
-        for (int j {0}; j < 4; ++j) {
-            // isolate that tile
-            int val {i & ~resetMasks[j]};
-            // shift to right
-            val >>= (j*4);
-            val = 0x1 << val;
-            val = (val == 1) ? 0 : val;
-            std::cout << val << "  ";
+template <typename T>
+uint16_t generateRowAndScore(uint16_t row, T& move) {
+    for (int i {move.start}; move.end(i); i = move.next(i)) {
+        int value {getMask(row, i)};
+        if (!value) {
+            move.nextEmptyPosition = (move.nextEmptyPosition == -1) ? i : move.nextEmptyPosition;
+        } else if (value == move.lastValue) {
+            row = merge(row, i, move);
+        } else {
+            move.lastValue = value;
+            move.lastValueIndex = i;
+            if (move.nextEmptyPosition != -1) {
+                row = swap(row, i, value, move);
+            }
         }
-        std::cout << "--->   ";
+    }
+    return row;
+}
 
-        uint16_t newRow {forwardTable[i]};
-        for (int j {0}; j < 4; ++j) {
-            // isolate that tile
-            int val {newRow & ~resetMasks[j]};
-            // shift to right
-            val >>= (j*4);
-            val = 0x1 << val;
-            val = (val == 1) ? 0 : val;
-            std::cout << val << "  ";
-        }
-        std::cout << "\n";
+
+void populateTables() {
+    for (int i {0}; i < 65536; ++i) {
+        Forward f;
+        Reverse r;
+        uint16_t row {static_cast<uint16_t>(i)};
+        forwardTable[i] = generateRowAndScore(row, f); 
+        reverseTable[i] = generateRowAndScore(row, r);
     }
 }
 
 int main() {
-    populateForwardTable();
-    populateReverseTable();
-    //printMoveRow();
-    /* 
+    populateTables();
+    std::ofstream out;
+    out.open("table.txt");
     for (int i {0}; i < 65536; ++i) {
-        std::cout << forwardTable[i] << '\n';
+        out << std::setfill('0') << std::setw(4) << std::hex << i << '\n';
+        out << "\tForward --> " << std::setfill('0') << std::setw(4) << std::hex << forwardTable[i] << '\n';
+        out << "\tReverse --> " << std::setfill('0') << std::setw(4) << std::hex << reverseTable[i] << "\n\n";
     }
-    */
     return 0;
 }
